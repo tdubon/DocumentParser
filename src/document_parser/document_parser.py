bcf25efa0bd8bbebc4615792b33341c09578ac5d
Tannia <34521046+tdubon@users.noreply.github.com>
2025-04-23 14:38:07 -0500

    Update document_parser.py
    updated regex pattern

--- src/document_parser/document_parser.py
+++ src/document_parser/document_parser.py
@@ -31,7 +31,7 @@ class Parser:
         self.sentence_holder = re.split(pattern, self.long_string) 
         
         # remove white spaces, meta characters, puctuation and lowercase  # [^\w]+[\s]|(\\t)|[()-?!.,:;]
-        pattern = r"(\\t)|(\\u)|[()-?[!#.',:;]" #remove meta characters and punctuation
+        pattern = r"(\\t)|(\\u)|[-]+|[]]|[()?[!#.',:;]" #remove meta characters and punctuation
         pattern2 = r'[^\w]+[\s]{2,}' #match any non-word character followed by 2 or more spaces
         for i in self.sentence_holder:
             i = re.sub(pattern, "", i).lower()
